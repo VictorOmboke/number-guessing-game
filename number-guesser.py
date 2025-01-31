f7756436afb80e42b08ec56152e5c0d266a11d89
Victor Omboke <victoromboke@gmail.com>
2025-01-30 20:32:33 -0800

    Import 'random' module and add function to generate random numbers.

---
+++ number-guesser.py
@@ -0,0 +1,10 @@
+# import 'random' module for random operations
+import random
+
+def create_random_num():
+    """
+    Generate a random integer between 1 and 100
+    """
+    random_int = random.randint(1, 100)
+    return random_int
+
