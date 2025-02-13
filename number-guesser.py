5c4b0e44cb0bd06f085b2dd833e5475c48374811
Victor Omboke <victoromboke@gmail.com>
2025-02-12 20:11:12 -0800

    Rework create_random_num function

--- number-guesser.py
+++ number-guesser.py
@@ -1,15 +1,6 @@
 # import 'random' module for random operations
 import random
 
-def create_random_num():
-    """
-    Generate a random integer between 1 and 100
-    """
-    random_int = random.randint(1, 100)
-    return random_int
-
-print(f"Random Number: {create_random_num()}")
-
 def get_upper_bound():
     """
     Prompt user to enter the upper limit of guessing game.
@@ -19,11 +10,21 @@ def get_upper_bound():
         num = input("Please enter a number greater than 1. ")
         # Check if all characters in num are digits and if the num is greater than 1.
         if num.isdigit() == True and int(num) > 1:
-            print(f"Your guess: {num}")
+            num = int(num)
             prompt = False
+            return num
         else:
             # Handle out of range and non-integer inputs
             print("Invalid input! Please try again.")
 
-get_upper_bound()
-            
+
+def create_random_num(upper_bound):
+    """
+    Generate a random integer between 1 and the users number.
+
+    Parameter: num - the upper limit of the game.
+    """
+    random_int = random.randint(1, upper_bound)
+    return random_int
+
+print(f"Random Number: {create_random_num(get_upper_bound())}")
